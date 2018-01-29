8f965a201dacc45ded0031ec23f0777ccccf0cb8
patrykpz3 <patrykpz333@gmail.ocm>
2018-01-29 21:22:36 +0100

    Dodaj pliki projektów.

---
+++ PRIME_T - Liczby Pierwsze/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIME_T___Liczby_Pierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                                                                                                    // Liczby w zakresie ze spoja
+            int n = 10000;
+            bool[] liczby = new bool[n + 1];
+            
+            for(int i=0; i <= n; i++)
+            {
+                liczby[i] = true;
+            }
+            liczby[1] = false;
+                                                                                                    // Próba wykorzystania sita Eratostenesa 
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (liczby[i] == true)
+                {
+                    for (int j = i + i; j <= n; j = j + i)
+                    {
+                        liczby[j] = false;
+                    }
+                }
+            }
+     
+            int ilosc_testow = Convert.ToInt32(Console.ReadLine());
+
+
+            for(int i=0; i < ilosc_testow; i++)
+            {
+                int liczba = Convert.ToInt32(Console.ReadLine());
+                if(liczby[liczba] == true)
+                {
+                    Console.WriteLine("TAK");
+                }
+                else
+                {
+                    Console.WriteLine("NIE");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ PRIME_T - Liczby Pierwsze/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("PRIME_T - Liczby Pierwsze")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PRIME_T - Liczby Pierwsze")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("90babb77-5611-4a97-a2ea-47dfd33a11a1")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
